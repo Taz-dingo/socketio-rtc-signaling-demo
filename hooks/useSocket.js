b1af6f262c71e1f40d0dc24ecdbe9df3956551a4
Taz-dingo <taste_my_blade@outlook.com>
2024-08-16 15:00:14 +0800

    refactor: rollback

--- hooks/useSocket.js
+++ hooks/useSocket.js
@@ -3,10 +3,10 @@ import { useEffect, useRef } from "react";
 
 const useSocket = () => {
   const socketCreated = useRef(false)
-  useEffect(() =>{
+  useEffect(() => {
     if (!socketCreated.current) {
       const socketInitializer = async () => {
-        await fetch ('/api/socket')
+        await fetch('/api/socket')
       }
       try {
         socketInitializer()
